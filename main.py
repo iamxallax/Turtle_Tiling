d9ef60dc1c88c83300a0c0be5d7c8c50f246ee5b
xallax <81593930+xallax-ekacnap@users.noreply.github.com>
2022-10-23 22:55:27 +0000

    made tiled square spirals work

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 from math import sin, cos, pi, asin
 import turtle
 
-t = turtle.Turtle()
-t.speed(0)
-t.penup()
-t.setx(-150)
-t.sety(-150)
-t.pendown()
-
-screen = turtle.Screen()
-
-
-def shape(s_length=200, sides=3):
-    degrees = (sides - 2)*180
-    t.color('black')
-    for i in range(sides):
-        t.forward(s_length)
-        t.left(180-(degrees/sides))
-
 def rad(deg):
     return deg*pi/180
 
+
 def deg(rad):
     return rad*180/pi
+    
+
+def shape(t, S=200, sides=3, inverted=False):
+    degrees = (sides - 2)*180
+    for i in range(sides):
+        t.forward(S)
+        if inverted:
+            t.right(180-(degrees/sides))
+        else:
+            t.left(180-(degrees/sides))
 
 def calc(S, F, sides=3):
     int_angle = (sides - 2)*180/sides
@@ -33,16 +27,38 @@ def calc(S, F, sides=3):
     A = deg(asin(G/D))
     return A, D
 
-def spiral(S, F, sides=3):
+def spiral(t, S, F, sides=3, inverted=False):
+    if inverted:
+        t.penup()
+        t.setheading(90)
+        t.forward(S)
+        t.pendown()
+        t.setheading(0)
     while int(S) > F:
-        shape(S, sides=sides)
+        shape(t, S, sides=sides, inverted=inverted)
         t.forward(F)
         A, S = calc(S, F, sides=sides)
-        t.left(A)
+        if inverted:
+            t.right(A)
+        else:
+            t.left(A)
+    t.setheading(0)
 
+screen = turtle.Screen()
+t = turtle.Turtle()
+t.speed(0)
+t.color('black')
 
-L = 200
-S = L
+S = 200
 F = 10
 
-spiral(S, F, sides=4)
+coords = [(0, 0, True), (0, -200, False), (-200, -200, True), (-200, 0, False), (-400, 0, True), (-400, -200, False), (200, -200, True), (200, 0, False)]
+
+#def make_grid(S, rows=2, cols=4):
+
+for x, y, inverted in coords:
+    t.penup()
+    t.setx(x)
+    t.sety(y)
+    t.pendown()
+    spiral(t, S, F, sides=4, inverted=inverted)
